a4c057dc862f6a325e80011d22717a17d0bb011f
Srikanth S <146076825+srikanth3608@users.noreply.github.com>
2025-08-12 19:29:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+let select = document.querySelectorAll('.currency');
+let input = document.getElementById('input');
+let err = document.getElementById('errMsg');
+
+// console.log(select);
+fetch('https://api.frankfurter.app/currencies')
+.then(res => res.json())
+.then(res => displayDown(res))
+function displayDown(res){
+    let curr = Object.entries(res);
+    // console.log(curr);
+    for(let i=0;i<curr.length;i++){
+        let opt = `<option value="${curr[i][0]}">${curr[i][0]}</option>`
+        select[0].innerHTML += opt;
+        select[1].innerHTML += opt;
+        // console.log(curr[i][0]);
+    }
+}
+
+let btn= document.getElementById('btn');
+btn.addEventListener('click',() => {
+    let curr1 = select[0].value;
+    let curr2 = select[1].value;
+    let inputVal = input.value;
+    if(curr1 === curr2){
+        err.innerHTML = "You have Entered same country";
+        err.style.color = 'red'
+    }
+    else{
+        convert(curr1,curr2,inputVal)
+    }
+});
+
+// convert(from, to, amount)
+function convert(curr1,curr2,inputVal) {
+    const host = 'api.frankfurter.app'
+    fetch(`https://${host}/latest?amount=${inputVal}&from=${curr1}&to=${curr2}`)
+    .then((res) => res.json())
+    .then((data) => {
+        let output = Object.values(data.rates)[0]
+        err.innerHTML = `The converted value is : ${output }`
+        err.style.color = 'blue'
+        console.log(output);
+        document.getElementById('result').value = output;
+    });
+}
+  
